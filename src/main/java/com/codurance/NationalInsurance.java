7f98a6774a549f8f6a354ca9e917ccaee11850e4
Sandro Mancuso <sandromancuso@yahoo.com>
2017-04-07 12:05:46 -0300

    Minor refactoring on NationalInsurance

--- src/main/java/com/codurance/NationalInsurance.java
+++ src/main/java/com/codurance/NationalInsurance.java
@@ -24,22 +24,21 @@ class NationalInsurance {
     }
 
     private BigDecimal calculateContribution() {
-        BigDecimal taxableAmountAtNormalRate = taxableAmountAtNormalRate();
-        BigDecimal taxableAmountAtHigherRate = taxableAmountAtHigherRate();
+        BigDecimal contributionAtNormalRate = contributionAtNormalRate();
+        BigDecimal contributionAtHigherRate = contributionAtHigherRate();
 
-        return multiply(taxableAmountAtNormalRate, NORMAL_CONTRIBUTION_RATE)
-                .add(multiply(taxableAmountAtHigherRate, HIGHER_CONTRIBUTION_RATE));
+        return contributionAtNormalRate.add(contributionAtHigherRate);
     }
 
-    private BigDecimal taxableAmountAtHigherRate() {
+    private BigDecimal contributionAtHigherRate() {
         BigDecimal taxableAmount = new BigDecimal(0);
         if (annualSalary.doubleValue() > HIGHER_RATE_SALARY.doubleValue()) {
             taxableAmount = annualSalary.subtract(HIGHER_RATE_SALARY);
         }
-        return taxableAmount;
+        return multiply(taxableAmount, HIGHER_CONTRIBUTION_RATE);
     }
 
-    private BigDecimal taxableAmountAtNormalRate() {
+    private BigDecimal contributionAtNormalRate() {
         BigDecimal taxableAmount = new BigDecimal(0);
         if (annualSalary.doubleValue() > MAX_SALARY_WITH_NO_CONTRIBUTION.doubleValue()) {
             BigDecimal taxableAtNormalRate = (annualSalary.doubleValue() > HIGHER_RATE_SALARY.doubleValue())
@@ -47,7 +46,7 @@ class NationalInsurance {
                                                     : annualSalary;
             taxableAmount = taxableAtNormalRate.subtract(MAX_SALARY_WITH_NO_CONTRIBUTION);
         }
-        return taxableAmount;
+        return multiply(taxableAmount, NORMAL_CONTRIBUTION_RATE);
     }
 
     private BigDecimal multiply(BigDecimal amount, double multiplicand) {
