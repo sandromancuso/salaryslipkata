ab8dbe884af2a8c28337d5565a70cf676ee3bba4
Sandro Mancuso <sandromancuso@yahoo.com>
2017-04-07 10:24:05 -0300

    Finished Iteration 4

--- src/main/java/com/codurance/TaxInfo.java
+++ src/main/java/com/codurance/TaxInfo.java
@@ -2,11 +2,17 @@ package com.codurance;
 
 import java.math.BigDecimal;
 
+import static java.math.BigDecimal.ROUND_HALF_UP;
+
 class TaxInfo {
 
     private static final BigDecimal NO_TAX = new BigDecimal(0);
     private static final BigDecimal MAX_TAX_FREE_ALLOWANCE = BigDecimal.valueOf(11000);
-    public static final BigDecimal TWENTY_PERCENT = BigDecimal.valueOf(0.20);
+    private static final BigDecimal HIGHER_SALARY_LIMIT = BigDecimal.valueOf(43000);
+
+    private static final BigDecimal NORMAL_TAX_RATE = BigDecimal.valueOf(0.20);
+    private static final BigDecimal HIGHER_TAX_RATE = BigDecimal.valueOf(0.40);
+
     private BigDecimal annualGrossSalary;
 
     TaxInfo(BigDecimal annualGrossSalary) {
@@ -26,9 +32,30 @@ class TaxInfo {
     }
 
     BigDecimal taxPayable() {
-        return (isGrossSalaryAboveMaxTaxFreeAllowance())
-                    ? taxPayableOf(TWENTY_PERCENT)
-                    : NO_TAX;
+        BigDecimal taxPayableAtNormalRate = taxPayableAtNormalRate();
+        BigDecimal taxPayableAtHigherRate = taxPayableAtHigherRate();
+
+        return taxPayableAtNormalRate.add(taxPayableAtHigherRate);
+    }
+
+    private BigDecimal taxPayableAtHigherRate() {
+        BigDecimal taxAtHigherRate = NO_TAX;
+        if (annualGrossSalary.doubleValue() > HIGHER_SALARY_LIMIT.doubleValue()) {
+            BigDecimal taxableAmount = annualGrossSalary.subtract(HIGHER_SALARY_LIMIT);
+            taxAtHigherRate = multiply(taxableAmount, HIGHER_TAX_RATE);
+        }
+        return taxAtHigherRate;
+    }
+
+    private BigDecimal taxPayableAtNormalRate() {
+        BigDecimal taxAtNormalRate = NO_TAX;
+         if (annualGrossSalary.doubleValue() > MAX_TAX_FREE_ALLOWANCE.doubleValue()) {
+            BigDecimal taxableAmount = (annualGrossSalary.doubleValue() > HIGHER_SALARY_LIMIT.doubleValue())
+                                            ? HIGHER_SALARY_LIMIT
+                                            : annualGrossSalary;
+            taxAtNormalRate = multiply(taxableAmount.subtract(MAX_TAX_FREE_ALLOWANCE), NORMAL_TAX_RATE);
+        }
+        return taxAtNormalRate;
     }
 
     private BigDecimal taxPayableOf(BigDecimal twentyPercent) {
@@ -38,4 +65,9 @@ class TaxInfo {
     private boolean isGrossSalaryAboveMaxTaxFreeAllowance() {
         return annualGrossSalary.compareTo(MAX_TAX_FREE_ALLOWANCE) == 1;
     }
+
+    private BigDecimal multiply(BigDecimal amount, BigDecimal multiplicand) {
+        return amount.multiply(multiplicand).setScale(2, ROUND_HALF_UP);
+    }
+
 }
--- src/test/java/com/codurance/TaxInfoShould.java
+++ src/test/java/com/codurance/TaxInfoShould.java
@@ -31,14 +31,32 @@ public class TaxInfoShould {
 
     @Test
     @Parameters({
-            "12000, 11000, 1000, 200.0",
-            "30000, 11000, 19000, 3800.0"
+            "12000, 11000, 1000, 200.00",
+            "30000, 11000, 19000, 3800.00",
+            "43000, 11000, 32000, 6400.00"
     })
     public void
     have_20_percent_tax_for_annual_salaries_between_11000_and_43000(BigDecimal annualGrossSalary,
                                                                     BigDecimal taxFreeAllowance,
                                                                     BigDecimal taxableIncome,
                                                                     BigDecimal taxPayable) {
+        TaxInfo taxInfo = new TaxInfo(annualGrossSalary);
+
+        assertThat(taxInfo.taxFreeAllowance()).isEqualTo(taxFreeAllowance);
+        assertThat(taxInfo.taxableIncome()).isEqualTo(taxableIncome);
+        assertThat(taxInfo.taxPayable()).isEqualTo(taxPayable);
+    }
+
+    @Test
+    @Parameters({
+            "45000, 11000, 34000, 7200.00",
+            "50000, 11000, 39000, 9200.00"
+    })
+    public void
+    have_40_percent_tax_for_annual_salaries_above_43000(BigDecimal annualGrossSalary,
+                                                        BigDecimal taxFreeAllowance,
+                                                        BigDecimal taxableIncome,
+                                                        BigDecimal taxPayable) {
         TaxInfo taxInfo = new TaxInfo(annualGrossSalary);
 
         assertThat(taxInfo.taxFreeAllowance()).isEqualTo(taxFreeAllowance);
